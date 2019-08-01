328582caf5d5535e84f58e0d4669a8c1dfe709e8
Pavel Kolomytkin <pavelkolomitkin@gmail.com>
2019-08-01 17:42:33 +0400

    add helmet; optimize process.env requests

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: process.env.APP_ENV !== 'prod' });
+
+  app.use(helmet());
   app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT || 3000);
